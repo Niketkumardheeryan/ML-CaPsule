b79faee51588b101b89702ffbbfcb272c7462b2e
Sakalya100 <70064084+Sakalya100@users.noreply.github.com>
2021-07-05 00:10:21 +0530

    Add files via upload

---
+++ Python Package/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+setup(name="package",
+version="0.3",
+description="This is a new package",
+long_description = "This is a very very long description",
+author="Sakalya",
+packages=['package'],
+install_requires=[])
