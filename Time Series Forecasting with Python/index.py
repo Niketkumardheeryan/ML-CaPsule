a1e87cf756c29b1eef491e0b7a9e23b889f0caba
durbar2003 <durbardibyo@gmail.com>
2022-03-10 03:18:29 +0530

    updated readme and commented code

--- Time Series Forecasting with Python/index.py
+++ Time Series Forecasting with Python/index.py
@@ -1,3 +1,7 @@
+"""
+dataset: GE.csv included in the directory
+"""
+
 import numpy as np
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM
@@ -7,27 +11,34 @@ from matplotlib import pyplot as plt
 from sklearn.preprocessing import StandardScaler
 import seaborn as sns
 
+#reading the csv file
 df = pd.read_csv('./GE.csv')
 print(df.head()) 
 
+#separating the dates for plotting
 train_dates = pd.to_datetime(df['Date'])
-print(train_dates.tail(15)) #Check last few dates. 
+print(train_dates.tail(15)) 
 
+#training variables
 cols = list(df)[1:6]
+
+#date and volume fields not used for training
 print(cols) 
 
+#new dataframe with only training data
 df_for_training = df[cols].astype(float)
 
+#normalize the dataset
 scaler = StandardScaler()
 scaler = scaler.fit(df_for_training)
 df_for_training_scaled = scaler.transform(df_for_training)
 
-
+#empty lists
 trainX = []
 trainY = []
 
-n_future = 1   
-n_past = 14  
+n_future = 1 #number of days we will be verifying   
+n_past = 14  #number of past days for prediction
 
 
 for i in range(n_past, len(df_for_training_scaled) - n_future +1):
@@ -39,7 +50,7 @@ trainX, trainY = np.array(trainX), np.array(trainY)
 print('trainX shape == {}.'.format(trainX.shape))
 print('trainY shape == {}.'.format(trainY.shape))
 
-
+#defining the autoencoder model
 model = Sequential()
 model.add(LSTM(64, activation='relu', input_shape=(trainX.shape[1], trainX.shape[2]), return_sequences=True))
 model.add(LSTM(32, activation='relu', return_sequences=False))
@@ -49,19 +60,20 @@ model.add(Dense(trainY.shape[1]))
 model.compile(optimizer='adam', loss='mse')
 model.summary()
 
-
+#fitting the model
 history = model.fit(trainX, trainY, epochs=5, batch_size=16, validation_split=0.1, verbose=1)
 
 plt.plot(history.history['loss'], label='Training loss')
 plt.plot(history.history['val_loss'], label='Validation loss')
 plt.legend()
- 
+
+#predicting 
 from pandas.tseries.holiday import USFederalHolidayCalendar
 from pandas.tseries.offsets import CustomBusinessDay
 us_bd = CustomBusinessDay(calendar=USFederalHolidayCalendar())
 
 n_past = 16
-n_days_for_prediction=15 
+n_days_for_prediction=15 #prediction for past 15 days
 
 predict_period_dates = pd.date_range(list(train_dates)[-n_past], periods=n_days_for_prediction, freq=us_bd).tolist()
 print(predict_period_dates)
@@ -71,7 +83,7 @@ prediction = model.predict(trainX[-n_days_for_prediction:])
 prediction_copies = np.repeat(prediction, df_for_training.shape[1], axis=-1)
 y_pred_future = scaler.inverse_transform(prediction_copies)[:,0]
 
-
+#converting timestamps to dates
 forecast_dates = []
 for time_i in predict_period_dates:
     forecast_dates.append(time_i.date())
