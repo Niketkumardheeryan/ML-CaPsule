01c86c5ea623cecaca4dc6c87b6f561778910427
Shivam Saxena <76241677+shivam-s16@users.noreply.github.com>
2022-03-31 11:22:04 +0530

    Add files via upload

---
+++ R language/Basic/Operators.R
@@ -0,0 +1,30 @@
+# The operators act on each element of the vector
+
+#Assign twwo vector
+> v <- c( 2,5.5,6)
+> t <- c(8, 3, 4)
+
+#Addition
+> c= t+v
+
+# Substraction
+> d = c-t
+
+# Multiplication
+> e = c*t
+
+# Division that is divide the first vector with the second
+> f = c/t
+
+#Give the remainder of the first vector with the second
+> g = c%%t
+
+#The result of division of first vector with second (quotient)
+>h = c%/%t
+
+# Raise to power
+> i = v^t
+
+'''Other operator like logical and relational are similar to other programming language'''
+
+
