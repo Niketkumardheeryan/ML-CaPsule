43d964e587b72fc0f083f639e3b21f25b72558fd
Sunkara Venkata Karthik Sai <venkatakarthiksai.s@gmail.com>
2024-10-25 12:33:59 +0530

    Added Weapon-Detection-System

---
+++ WEAPON-DETECTION-SYSTEM/weapon_detection1.py
@@ -0,0 +1,74 @@
+import cv2
+import numpy as np
+
+# Load Yolo
+net = cv2.dnn.readNet("yolov3_training_2000.weights", "yolov3_testing.cfg")
+net.setPreferableBackend(cv2.dnn.DNN_BACKEND_DEFAULT)
+net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
+classes = ["Weapon"]
+
+# Open the camera
+cap = cv2.VideoCapture(0)
+
+if not cap.isOpened():
+    print("Error: Could not open camera.")
+    exit()
+
+while True:
+    ret, img = cap.read()
+    
+    if not ret:
+        print("Error: Could not read frame from camera.")
+        break
+    
+    height, width, channels = img.shape
+
+    # Detecting objects
+    blob = cv2.dnn.blobFromImage(img, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+    net.setInput(blob)
+    
+    layer_names = net.getLayerNames()
+    output_layers = [layer_names[i - 1] for i in net.getUnconnectedOutLayers()]
+    colors = np.random.uniform(0, 255, size=(len(classes), 3))
+    outs = net.forward(output_layers)
+
+    # Showing information on the screen
+    class_ids = []
+    confidences = []
+    boxes = []
+    for out in outs:
+        for detection in out:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            if confidence > 0.5:
+                # Object detected
+                center_x = int(detection[0] * width)
+                center_y = int(detection[1] * height)
+                w = int(detection[2] * width)
+                h = int(detection[3] * height)
+
+                # Rectangle coordinates
+                x = int(center_x - w / 2)
+                y = int(center_y - h / 2)
+
+                boxes.append([x, y, w, h])
+                confidences.append(float(confidence))
+                class_ids.append(class_id)
+
+    indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+    if len(indexes) > 0:
+        for i in indexes.flatten():
+            x, y, w, h = boxes[i]
+            label = str(classes[class_ids[i]])
+            color = colors[class_ids[i]]
+            cv2.rectangle(img, (x, y), (x + w, y + h), color, 2)
+            cv2.putText(img, label, (x, y + 30), cv2.FONT_HERSHEY_PLAIN, 3, color, 3)
+
+    cv2.imshow("Image", img)
+    key = cv2.waitKey(1)
+    if key == 27:  # Press 'Esc' key to break the loop
+        break
+
+cap.release()
+cv2.destroyAllWindows()
