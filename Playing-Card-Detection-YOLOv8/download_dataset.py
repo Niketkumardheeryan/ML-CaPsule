4c72463004b10fe68a114019cafa4b0f5eebab12
aaniya22 <aaniyaatomar@gmail.com>
2026-07-16 22:58:23 +0530

    Remove download_dataset.py, logic already merged into notebook

--- Playing-Card-Detection-YOLOv8/download_dataset.py
+++
@@ -1,6 +0,0 @@
-from roboflow import Roboflow
-
-rf = Roboflow(api_key="j29PBczQJNP6Rq3uOV0V")
-project = rf.workspace("0lauk0").project("playing-cards-muou8")
-version = project.version(10)
-dataset = version.download("yolov8")
